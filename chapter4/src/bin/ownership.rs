5a0cef38effd9299aa576ba071aa0a190ae8f3cc
Phi <aiwavision@protonmail.com>
2021-12-12 13:50:22 -0300

    more ownership tests

--- chapter4/src/bin/ownership.rs
+++ chapter4/src/bin/ownership.rs
@@ -3,8 +3,14 @@ fn main() {
 }
 
 fn ownership() {
-    let s = "A test string";
-    let v = s;
-    println!("v: {}", v);
-    println!("s: {}", s);
+    let s1 = String::from("A test string");
+    let s2 = s1.clone();
+    println!("s1: {:p}", &s1);
+    println!("s2: {:p}", &s2);
+
+    let v1 = vec![1, 2, 3];
+    let mut v2 = v1.clone();
+    v2[0] = 5;
+    println!("v1: {:?}", v1);
+    println!("v2: {:?}", v2);
 }
